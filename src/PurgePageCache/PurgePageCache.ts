2882bcdccf497436729dd06aef411d1e864f8abd
海屿有燕 <zorua@vip.qq.com>
2026-02-19 00:18:11 +0800

    feat: add PurgePageCache UTCLiveClock HanSect and Toastify

---
+++ src/PurgePageCache/PurgePageCache.ts
@@ -0,0 +1,38 @@
+import {getBody} from 'ext.gadget.Util';
+import {getMessage} from './modules/i18n';
+import {purge} from './modules/purge';
+
+void getBody().then(function purgePageCache($body: JQuery<HTMLBodyElement>): void {
+	const {wgAction, wgPageName, wgNamespaceNumber} = mw.config.get();
+
+	if (wgAction !== 'view' || wgNamespaceNumber < 0) {
+		return;
+	}
+
+	const portletId: 'p-cactions' | 'p-tb' = $body.find('#p-cactions').length ? 'p-cactions' : 'p-tb';
+	const portletElement: HTMLLIElement | null = mw.util.addPortletLink(
+		portletId,
+		'#',
+		getMessage('Purge'),
+		'ca-purge',
+		getMessage('PurgeFromServer')
+	);
+	if (!portletElement) {
+		return;
+	}
+
+	(portletElement.querySelector('a') as HTMLAnchorElement).addEventListener('click', (event: MouseEvent): void => {
+		event.preventDefault();
+		void purge(wgPageName);
+	});
+
+	for (const element of $body.find<HTMLAnchorElement>('a[href*="action=purge"]')) {
+		const title: string = mw.util.getParamValue('title', element.href) || wgPageName;
+		element.addEventListener('click', (event: MouseEvent): void => {
+			event.preventDefault();
+			void purge(title);
+		});
+	}
+});
+
+export {purge} from './modules/purge';
